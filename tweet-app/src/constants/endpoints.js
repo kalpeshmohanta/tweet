25a1f5602d37e2dfa03c349ca2b855c3109a3ee1
kalpeshmohanta <kalpeshmohanta@gmail.com>
2022-09-28 21:37:30 +0530

    cc

---
+++ tweet-app/src/constants/endpoints.js
@@ -0,0 +1,9 @@
+export const BASE_URI ='http://13.59.139.84:8000'
+export const BASE_TWEET_URL = '/api/v1.0/tweets'
+export const FORGOT_PASSWD = '/forgetPassword'
+export const AUTHENTICATE = '/authenticate'
+export const LOGOUT = '/logout/' 
+export const REGISTER = '/register' 
+export const ALL_TWEETS = '/all'
+export const ALL_USERS = '/users/all'
+export const GET_USER = "/api/v1.0/user/search/"
